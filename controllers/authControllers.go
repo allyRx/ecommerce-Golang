43e1ecd6fe542aaafc76e37e4bd7858ec977775d
allyRx <herindrainyall2003@gmail.com>
2025-10-07 01:11:27 +0300

    register features

--- controllers/authControllers.go
+++ controllers/authControllers.go
@@ -4,6 +4,7 @@ import (
 	"github.com/allyRx/ecommerce-Golang/database"
 	"github.com/allyRx/ecommerce-Golang/models"
 	"github.com/gofiber/fiber/v3"
+	"golang.org/x/crypto/bcrypt"
 )
 
 
@@ -12,12 +13,24 @@ func Register(c fiber.Ctx) error {
   
 	data := new(models.Customer)
 
+	// Bind les données JSON du corps dans la struct
 	if err := c.Bind().Body(data); err != nil{
 		return  c.Status(500).JSON(fiber.Map{"message" : "Bad Request" + err.Error(),
 	})
 	}
+	
+
+	// Hasher le mot de passe seulement après avoir reçu la donnée
+	 passwordCrypter , err := bcrypt.GenerateFromPassword([]byte(data.Password) , 12)
+
+	 if err != nil {
+		return  c.Status(500).JSON(fiber.Map{"Message" : "Erreur cryptage" + err.Error()})
+	}
 
 
+	// Stocker le mot de passe hashé en tant que []byte
+	data.Password = string(passwordCrypter)
+	
 	
  	if err := database.DB.Create(data).Error;  err != nil{
 		return c.Status(500).JSON(fiber.Map{"Message" : "Il y a une erreur critique" + err.Error(),})
